940feffde8cead5de8083df649071ee7a1b86ba7
Ernesto <ernesto.dpa28@gmail.com>
2026-07-03 21:44:49 -0500

    Corregir configuracion PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,24 +1,14 @@
-const CACHE_NAME = "miapp-cache-v1";
-
-const urlsToCache = [
-  "/",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
+const CACHE_NAME = "jd-gym-cache-v1";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  console.log("Service Worker instalado");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  console.log("Service Worker activado");
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
